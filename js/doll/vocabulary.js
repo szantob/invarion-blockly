cff2f3877a8b9e6d6fb4a365bf00695bbd47f82e
szantob <szantobenedek@gmail.com>
2020-05-19 03:29:08 +0200

    Cleanup #1

--- js/doll/vocabulary.js
+++ js/doll/vocabulary.js
@@ -10,7 +10,7 @@ var Vocabulary = {
 	getNameListForBlocklyNode : function(){
 		var list = []
 		for (var i = 0; i < this.itemArray.length; i++) {
-			list.push(this.itemArray[i].toUpperCase());
+			list.push(this.itemArray[i]);
 		}
 		return list;
 	},
@@ -18,12 +18,10 @@ var Vocabulary = {
 		this.itemArray.push(itemName);
 	}
 }
-
 Blockly.Blocks['vocabulary_node'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField(new Blockly.FieldDropdown(this.generateOptions), "NAME");
-    this.appendDummyInput()
+        .appendField(new Blockly.FieldLabelSerializable(""), "NAME")
         .appendField(":")
         .appendField(new Blockly.FieldTextInput("description"), "description");
     this.setInputsInline(true);
@@ -32,22 +30,6 @@ Blockly.Blocks['vocabulary_node'] = {
     this.setColour(20);
  this.setTooltip("");
  this.setHelpUrl("");
-  },
-  generateOptions: function() {
-    return Vocabulary.getNameListForBlocklyItem();
-  }
-};
-Blockly.Blocks['vocabulary_item'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldDropdown(this.generateOptions), "ITEM");
-    this.setOutput(true, null);
-    this.setColour(20);
- this.setTooltip("");
- this.setHelpUrl("");
-  },
-  generateOptions: function() {
-    return Vocabulary.getNameListForBlocklyItem();
   }
 };
 
@@ -57,21 +39,13 @@ var vocabularyCallback = function(workspace) {
   var block = Blockly.Xml.textToDom('<button text="New entry" callbackKey="vocabularyNewEntry"></button>');
   xmlList.push(block);
   if(vocabularyList.length == 0) return xmlList;
-  var block = Blockly.Xml.textToDom(
-	'<block type="vocabulary_node">'+
-		'<field name="NAME">' + vocabularyList[0] + '</field>' +
-	'</block>'
-  );
-  xmlList.push(block);
-  if (Blockly.Blocks['vocabulary_item']) {
-	for (var i = 0; i < vocabularyList.length; i++) {
-	  var blockText = 
-		'<block type="vocabulary_item">'+
-		'<field name="ITEM">' + vocabularyList[i] + '</field>' +
-		'</block>';
-	  var block = Blockly.Xml.textToDom(blockText);
-	  xmlList.push(block);
-	}
+  for (var i = 0; i < vocabularyList.length; i++) {
+	var blockText = 
+	  '<block type="vocabulary_node">'+
+		'<field name="NAME">' + vocabularyList[i] + '</field>' +
+	  '</block>';
+	var block = Blockly.Xml.textToDom(blockText);
+	xmlList.push(block);
   }
   return xmlList;
 };
---
+++ js/frontend.js
@@ -0,0 +1,28 @@
+var toolbox = document.getElementById("toolbox");
+var workspace = Blockly.inject('blocklyDiv', options);
+var options = { 
+	toolbox : toolbox, 
+	collapse : true, 
+	comments : true, 
+	disable : true, 
+	maxBlocks : Infinity, 
+	trashcan : true, 
+	horizontalLayout : false, 
+	toolboxPosition : 'start', 
+	css : true, 
+	media : 'https://blockly-demo.appspot.com/static/media/', 
+	rtl : false, 
+	scrollbars : true, 
+	sounds : true, 
+	oneBasedIndex : true
+};
+var initialize = function(){
+	workspace.registerToolboxCategoryCallback('VOCABULARY', vocabularyCallback);
+	workspace.registerToolboxCategoryCallback('TAXONOMY', taxonomyCallback);
+	workspace.registerToolboxCategoryCallback('DATAMODEL', datamodelCallback);
+	workspace.registerButtonCallback('vocabularyNewEntry', vocabularyNewEntryCallback);
+	workspace.registerButtonCallback('taxonomyNewEntry', taxonomyNewEntryCallback);
+	var workspaceBlocks = document.getElementById("workspaceBlocks"); 
+	Blockly.Xml.domToWorkspace(workspaceBlocks, workspace);
+	toolboxUpdate();
+}
